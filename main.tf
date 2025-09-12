e8ccb07c92ebabef7dcce7089ad57e8fdf93e934
martin Raju <martin.raju313@gmail.com>
2025-09-12 14:02:48 +0530

    updated

--- main.tf
+++ main.tf
@@ -45,9 +45,9 @@ data "aws_subnet_ids" "default" {
 # Spot Instance
 # -------------------------
 resource "aws_instance" "spot_worker" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = element(data.aws_subnet_ids.default.ids, 0)
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = element(data.aws_subnet_ids.default.ids, 0)
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   key_name               = "test01"
 
