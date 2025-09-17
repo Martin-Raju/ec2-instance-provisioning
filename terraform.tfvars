2ca5939acf66481559bc24f76dbacdbf2aebb8b7
martin Raju <martin.raju313@gmail.com>
2025-09-17 15:03:32 +0530

    updated

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,12 @@
 
-aws_region     = "us-east-1"
-ami_id         = "ami-0b09ffb6d8b58ca91"
-instance_type  = "t3a.micro"
-max_spot_price = "0.10" # max price in USD/hour
-environment    = "dev"
-key_name       = "test01"
+aws_region                               = "us-east-1"
+ami_id                                   = "ami-0b09ffb6d8b58ca91"
+key_name                                 = "test01"
+environment                              = "dev"
+asg_min_size                             = 1
+asg_max_size                             = 4
+asg_desired_capacity                     = 2
+instance_types                           = ["t3.medium", "t3.large"]
+default_instance_type                    = "t3.medium"
+on_demand_percentage_above_base_capacity = 50
+cpu_target_value                         = 50
--- variables.tf
+++ variables.tf
@@ -1,29 +1,54 @@
 variable "aws_region" {
-  description = "aws region"
+  description = "AWS region"
   type        = string
 }
 
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "AMI ID for the EC2 instances"
   type        = string
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "max_spot_price" {
-  description = "Maximum price for spot instance"
+variable "key_name" {
+  description = "Key pair name for SSH access"
   type        = string
 }
 
 variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
   type        = string
-  description = "dev"
 }
 
-variable "key_name" {
+variable "asg_min_size" {
+  description = "Minimum size of the Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_max_size" {
+  description = "Maximum size of the Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_desired_capacity" {
+  description = "Desired capacity of the Auto Scaling Group"
+  type        = number
+}
+
+variable "instance_types" {
+  description = "List of instance types for the Mixed Instances ASG"
+  type        = list(string)
+}
+
+variable "default_instance_type" {
+  description = "Default instance type for Launch Template (must be in instance_types)"
   type        = string
-  description = "Key pair name for SSH access"
+}
+
+variable "on_demand_percentage_above_base_capacity" {
+  description = "Percentage of on-demand capacity above base in Mixed Instances Policy"
+  type        = number
+}
+
+variable "cpu_target_value" {
+  description = "CPU utilization percentage target for scaling policy"
+  type        = number
 }
