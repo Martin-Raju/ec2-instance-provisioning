adec68ede49380e3d605bba148576ca16409241e
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-09-17 11:46:39 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,15 @@
 # -------------------------
 # Output
 # -------------------------
-output "instance_ids" {
-  description = "IDs of all Spot instances"
-  value = {
-    for k, m in module.spot_instance : k => m.id
-  }
+output "spot_request_ids" {
+  value = { for k, m in module.spot_instance : k => m.spot_instance_request_id }
+}
+
+output "spot_instance_ids" {
+  value = { for k, m in module.spot_instance : k => m.spot_instance_id }
 }
 
 output "instance_public_ips" {
-  description = "Public IPs of all Spot instances"
-  value = {
-    for k, m in module.spot_instance : k => m.public_ip
-  }
+  value = { for k, m in module.spot_instance : k => m.public_ip }
 }
 
