079c83e071125ebf9f5ab6a973a19cc0400c43cd
martin Raju <martin.raju313@gmail.com>
2025-09-17 14:50:57 +0530

    updated

--- main.tf
+++ main.tf
@@ -46,7 +46,6 @@ module "security_group" {
 resource "aws_launch_template" "asg_lt" {
   name_prefix            = "asg-lt"
   image_id               = var.ami_id
-  instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [module.security_group.security_group_id]
   network_interfaces {
