39374b6e20c8654e97235bf7c7d95a63610a5276
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-09-17 11:56:29 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,6 @@
 # -------------------------
 # Output
 # -------------------------
-output "spot_request_ids" {
-  value = { for k, m in module.spot_instance : k => m.spot_instance_request_id }
-}
 
 output "spot_instance_ids" {
   value = { for k, m in module.spot_instance : k => m.spot_instance_id }
