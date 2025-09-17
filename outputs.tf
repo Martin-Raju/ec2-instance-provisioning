6897f8d60b3e452840279475e770044a9b8a50b4
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-09-17 11:58:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,16 @@
 # Output
 # -------------------------
 
-output "spot_instance_ids" {
-  value = { for k, m in module.spot_instance : k => m.spot_instance_id }
+output "spot_ids" {
+  description = "EC2 instance IDs or Spot Request IDs depending on state"
+  value = {
+    for k, m in module.spot_instance : k => m.id
+  }
 }
 
 output "instance_public_ips" {
-  value = { for k, m in module.spot_instance : k => m.public_ip }
+  description = "Public IPs of the instances"
+  value = {
+    for k, m in module.spot_instance : k => m.public_ip
+  }
 }
-
