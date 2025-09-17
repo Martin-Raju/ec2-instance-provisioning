008ed8808345421aefa35f35a44467f05180dff6
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-09-17 18:00:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,6 +46,7 @@ module "security_group" {
 resource "aws_launch_template" "spot_lt" {
   name_prefix            = "spot-lt"
   image_id               = var.ami_id
+  instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [module.security_group.security_group_id]
 
