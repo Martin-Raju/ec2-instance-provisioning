2d6c94cd5a315d27bc17aa8b608b5afcb0b1e3c6
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-09-18 15:21:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,6 @@ module "asg" {
     stress --cpu 3 --timeout 600 &
   EOT
   )
-  instance_types = var.instance_types
   # Mixed Instances Policy
   mixed_instances_policy = {
     launch_template = {
