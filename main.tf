af143183e26b1a521835057c63b9cfa1bb33f2f3
martin Raju <martin.raju313@gmail.com>
2025-09-19 18:21:56 +0530

    updated

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ module "asg" {
   mixed_instances_policy = {
     instances_distribution = {
       base_capacity                            = 1
-      on_demand_percentage_above_base_capacity = 30
+      on_demand_percentage_above_base_capacity = var.on_demand_percentage_above_base_capacity
       spot_allocation_strategy                 = "capacity-optimized"
     }
 
