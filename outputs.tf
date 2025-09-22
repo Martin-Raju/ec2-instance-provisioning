a693ec003d74f424d26e2860dc6e13a54de9810f
martin Raju <martin.raju313@gmail.com>
2025-09-22 11:04:31 +0530

    updated

--- outputs.tf
+++ outputs.tf
@@ -6,19 +6,11 @@ output "asg_id" {
   description = "The ID of the Auto Scaling Group"
   value       = module.asg.autoscaling_group_id
 }
-
 # Launch Template ID 
 output "launch_template_id" {
   description = "The ID of the Launch Template"
   value       = module.asg.launch_template_id
 }
-
-# ASG Instance IDs
-output "asg_instance_ids" {
-  description = "List of instance IDs in the Auto Scaling Group"
-  value       = module.asg.instances
-}
-
 # Security Group ID
 output "security_group_id" {
   description = "The ID of the security group created"
