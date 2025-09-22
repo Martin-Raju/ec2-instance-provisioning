8fe95ad35a411b70ebc6e8509ca39fd2cc4f4b36
martin Raju <martin.raju313@gmail.com>
2025-09-22 12:36:16 +0530

    updated

--- main.tf
+++ main.tf
@@ -57,11 +57,11 @@ module "asg" {
   health_check_grace_period = 300
 
   # --- Launch Template parameters ---
-  create_launch_template     = true
-  force_delete               = true
-  launch_template_name       = "spot-lt"
-  image_id                   = var.ami_id
-  instance_type              = var.default_instance_type
+  create_launch_template = true
+  force_delete           = true
+  launch_template_name   = "spot-lt"
+  image_id               = var.ami_id
+  #instance_type              = var.default_instance_type
   key_name                   = var.key_name
   security_groups            = [module.security_group.security_group_id]
   use_mixed_instances_policy = true
@@ -73,15 +73,16 @@ module "asg" {
   )
   mixed_instances_policy = {
     instances_distribution = {
-      base_capacity                            = 0
+      base_capacity                            = 1
       on_demand_percentage_above_base_capacity = var.on_demand_percentage_above_base_capacity
       spot_allocation_strategy                 = "lowest-price"
+      spot_instance_pools                      = 3
     }
 
     launch_template = {
       launch_template_specification = {
-        launch_template_id = module.asg.launch_template_id
-        version            = "$Latest"
+        #launch_template_id = module.asg.launch_template_id
+        version = "$Latest"
       }
       overrides = [
         { instance_type = "t4g.micro" },
