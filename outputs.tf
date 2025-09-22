9355dd6beadd200285a18feb998c56850e85fdda
martin Raju <martin.raju313@gmail.com>
2025-09-22 11:02:22 +0530

    updated

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,26 @@
 # -------------------------
 # Output
 # -------------------------
-# --- VPC and Subnets ---
-output "vpc_id" {
-  description = "Default VPC ID"
-  value       = data.aws_vpc.default.id
-}
-
-output "subnet_ids" {
-  description = "IDs of the default subnets"
-  value       = data.aws_subnets.default.ids
-}
-
-# --- Security Group ---
-output "security_group_id" {
-  description = "Security Group ID allowing SSH access"
-  value       = module.security_group.security_group_id
-}
-
-# --- Auto Scaling Group ---
-output "asg_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = module.asg.autoscaling_group_name
-}
-
-output "asg_arn" {
-  description = "ARN of the Auto Scaling Group"
-  value       = module.asg.autoscaling_group_arn
-}
-
+# ASG ID
 output "asg_id" {
-  description = "ID of the Auto Scaling Group"
+  description = "The ID of the Auto Scaling Group"
   value       = module.asg.autoscaling_group_id
 }
 
-# --- Launch Template ---
+# Launch Template ID 
 output "launch_template_id" {
-  description = "ID of the Launch Template created by the ASG module"
-  value       = module.asg.launch_template[0].id
+  description = "The ID of the Launch Template"
+  value       = module.asg.launch_template_id
 }
 
-output "launch_template_name" {
-  description = "Name of the Launch Template created by the ASG module"
-  value       = module.asg.launch_template[0].name
+# ASG Instance IDs
+output "asg_instance_ids" {
+  description = "List of instance IDs in the Auto Scaling Group"
+  value       = module.asg.instances
 }
 
-# --- Mixed Instances ---
-output "mixed_instances_distribution" {
-  description = "Instances distribution configuration for the mixed instances policy"
-  value       = module.asg.mixed_instances_policy
+# Security Group ID
+output "security_group_id" {
+  description = "The ID of the security group created"
+  value       = module.security_group.security_group_id
 }
-
