b0ed768bf72607c9c66d6a08eebbc34c82b89361
martin Raju <martin.raju313@gmail.com>
2025-09-23 12:33:33 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ module "security_group" {
       protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
       description = "SSH"
-    }
+    },
     {
       from_port   = 80
       to_port     = 80
