8b775ed84f3d5edbdc7c139bd137b23350649c66
martin Raju <martin.raju313@gmail.com>
2025-09-24 11:59:33 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ module "security_group" {
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = "0.0.0.0/0"
       description = "HTTP"
     }
   ]
