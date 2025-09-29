06f13d9cdd557af733e180f060e4fa6525c96dd2
martin Raju <martin.raju313@gmail.com>
2025-09-29 16:43:13 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -170,12 +170,18 @@ module "asg" {
 }
 
 locals {
-  alb_arn              = var.create_alb ? module.alb[0].lb_arn : data.aws_lb.existing[0].arn
-  alb_target_group_arn = var.create_alb ? module.alb[0].target_group_arns[0] : data.aws_lb_target_group.existing[0].arn
+  alb_arn = var.create_alb
+    ? module.alb[0].lb_arn
+    : try(data.aws_lb.existing[0].arn, null)
+
+  alb_target_group_arn = var.create_alb
+    ? module.alb[0].target_group_arns[0]
+    : try(data.aws_lb_target_group.existing[0].arn, null)
 }
 # --- Attach ASG to Target Group ---
 
 resource "aws_autoscaling_attachment" "asg_alb" {
-  autoscaling_group_name = module.asg.autoscaling_group_name
-  lb_target_group_arn    = local.alb_target_group_arn
+  count                   = local.alb_target_group_arn != null ? 1 : 0
+  autoscaling_group_name  = module.asg.autoscaling_group_name
+  lb_target_group_arn     = local.alb_target_group_arn
 }
