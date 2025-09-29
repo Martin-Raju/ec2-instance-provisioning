d05c509f2bfd13d33d393f23bb7aaac137d5f689
martin Raju <martin.raju313@gmail.com>
2025-09-29 17:46:34 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,8 +142,8 @@ module "asg" {
     }
 
     override = [
-      #    { instance_type = var.instance_type_p1, spot_price = var.spot_price_p1 },
-      { instance_type = var.instance_type_p2, spot_price = var.spot_price_p2 },
+      { instance_type = var.instance_type_p1, spot_price = var.spot_price_p1 },
+     #{ instance_type = var.instance_type_p2, spot_price = var.spot_price_p2 },
       { instance_type = var.instance_type_p3, spot_price = var.spot_price_p3 },
       { instance_type = var.instance_type_p4, spot_price = var.spot_price_p4 }
     ]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,15 +33,15 @@ variable "asg_desired_capacity" {
   type        = number
 }
 
-#variable "instance_type_p1" {
-#  description = "List of instance types for the Mixed Instances ASG"
-# type        = string
-#}
-
-variable "instance_type_p2" {
+variable "instance_type_p1" {
   description = "List of instance types for the Mixed Instances ASG"
   type        = string
 }
+
+#variable "instance_type_p2" {
+#  description = "List of instance types for the Mixed Instances ASG"
+#  type        = string
+#}
 
 variable "instance_type_p3" {
   description = "List of instance types for the Mixed Instances ASG"
@@ -68,10 +68,10 @@ variable "cpu_target_value" {
   type        = number
 }
 
-#variable "spot_price_p1" {
-#  description = "Default instance type for Launch Template "
-#  type        = number
-#}
+variable "spot_price_p1" {
+  description = "Default instance type for Launch Template "
+  type        = number
+}
 
 variable "spot_price_p2" {
   description = "Default instance type for Launch Template "
