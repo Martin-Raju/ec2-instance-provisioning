60d1764cd350789da2627d6a0d28553a143bb2d3
martin Raju <martin.raju313@gmail.com>
2025-10-01 15:51:09 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -155,10 +155,10 @@ resource "null_resource" "delete_old_asgs" {
         --force-delete
     EOT
   }
-  
+
   triggers = {
     asg_count = length(data.aws_autoscaling_groups.existing.names)
-  }  
+  }
 
   depends_on = [data.aws_autoscaling_groups.existing]
 }
