4544ef1b80d33e6a7020f0d418290e6f3d13e2be
martin Raju <martin.raju313@gmail.com>
2025-10-03 11:51:03 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -297,7 +297,7 @@ resource "aws_autoscaling_group" "this" {
       min_healthy_percentage = 90
       instance_warmup        = 100
     }
-    triggers = [aws_launch_template.web_lt.id]
+    triggers = [aws_launch_template.web_lt.arn]
   }
 }
 
