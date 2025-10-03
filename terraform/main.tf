296877b4e0b31833dfbd89f355488f07efeee091
martin Raju <martin.raju313@gmail.com>
2025-10-03 15:33:43 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,18 +108,11 @@ module "asg" {
   health_check_grace_period  = 300
   create_launch_template     = true
   force_delete               = true
-  launch_template_name       = "spot-lt"
+  launch_template_name       = "Test-Web-lt"
   image_id                   = aws_ami_from_instance.web_ami.id
   key_name                   = var.key_name
   security_groups            = [module.security_group.security_group_id]
   use_mixed_instances_policy = true
-
-  user_data = base64encode(<<-EOT
-    #!/bin/bash
-    yum install -y stress
-    stress --cpu 3 --timeout 600 &
-  EOT
-  )
 
   mixed_instances_policy = {
     instances_distribution = {
