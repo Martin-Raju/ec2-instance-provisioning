4f74a9db773e1e0fa6b4f6d0c2368151f0f826cf
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-10-03 17:14:35 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "my-tfstate-bucket-0123456"
-    key     = "ENV/Test/terraform.tfstate"
+    key     = "ENV/Develop/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
